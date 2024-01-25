4c105321552f9aadae9342265068d9c9b2d142cd
Tommi Frykman <Tommi.Frykman@elev.ga.ntig.se>
2024-01-25 14:08:06 +0100

    prprprp

---
+++ RestTinface/Charachter.cs
@@ -0,0 +1,15 @@
+﻿
+
+public class Character
+{
+    public string name { get; set;}
+
+    public int age { get; set; }
+    public string bounty { get; set; }
+
+    public string birthday { get; set; }
+
+
+
+
+}
--- RestTinface/Program.cs
+++ RestTinface/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using RestSharp;
+using System.Text.Json;
+
+RestClient client = new("https://onepiecelogs.dev/api");
+
+
+string character = "roronoa_zoro";
+RestRequest request = new($"characters/{character}");
+
+RestResponse response = client.GetAsync(request).Result;
+
+// Console.WriteLine(response.Content);
+
+Character p = JsonSerializer.Deserialize<Character>(response.Content);
+
+
+
+Console.WriteLine(p.name);
+Console.WriteLine(p.age);
+Console.WriteLine(p.bounty);
+Console.WriteLine(p.birthday);
+Console.ReadLine();
